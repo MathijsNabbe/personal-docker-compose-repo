c8721ac09f4587e0f9242ba976ca497bcf71f9cf
MathijsNabbe <datenenaampje@gmail.com>
2025-04-09 14:43:29 +0200

    Initialize GitHub Actions Runner configuration files

---
+++ Github Actions Runner/README.md
@@ -0,0 +1,6 @@
+# Environment Variables
+| Variable | Description |
+| :------: | ----------- |
+| RUNNER_URL | The URL of the repository the runner will be active in. |
+| RUNNER_TOKEN | The token for the new runner, activated on Github. |
+| RUNNER_NAME | The name for the runner. |
---
+++ Github Actions Runner/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Configuring GitHub Actions Runner..."
+
+./config.sh \
+  --url "$REPO_URL" \
+  --token "$RUNNER_TOKEN" \
+  --name "$RUNNER_NAME" \
+  --work "_work" \
+  --unattended \
+  --replace
+
+cleanup() {
+  echo "Removing runner..."
+  ./config.sh remove --unattended --token "$RUNNER_TOKEN"
+  exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+echo "Starting runner..."
+./run.sh
